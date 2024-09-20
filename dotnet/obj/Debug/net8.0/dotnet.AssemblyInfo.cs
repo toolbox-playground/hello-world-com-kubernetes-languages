a031a1b6437ec274416ffb8ebfb719f3b736711c
Marcelo Buzzetti <marcelobuzzetti@toolboxdevops.cloud>
2024-09-20 09:31:17 -0300

    img

--- dotnet/obj/Debug/net8.0/dotnet.AssemblyInfo.cs
+++ dotnet/obj/Debug/net8.0/dotnet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fcc496a2e7e325a180803c407595f34b6d023d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2cca89a15fff931ef9de2c31dd5efd84f58c4f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
